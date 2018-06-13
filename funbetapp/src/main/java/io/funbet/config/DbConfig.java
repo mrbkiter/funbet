63acc85dacd76bd54c4a44939ad194ccc619bea8
Vu Nguyen <vu.nguyen@hrboss.com>
2018-06-14 00:30:31 +0700

    add flyway and db

--- funbetapp/src/main/java/io/funbet/config/DbConfig.java
+++ funbetapp/src/main/java/io/funbet/config/DbConfig.java
@@ -1,8 +1,11 @@
 package io.funbet.config;
 
+import io.funbet.repository.Repository;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaRepositories(basePackageClasses = Repository.class)
 public class DbConfig
 {
 }
--- funbetapp/src/main/java/io/funbet/model/entity/MatchEntity.java
+++ funbetapp/src/main/java/io/funbet/model/entity/MatchEntity.java
@@ -1,9 +1,13 @@
 package io.funbet.model.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity(name = "macth_bet")
+@Getter @Setter
 public class MatchEntity
 {
     @Id
