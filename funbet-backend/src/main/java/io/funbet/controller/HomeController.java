483d2d89a30a67ad8eaaa7d89c478232fb8cb293
Nguyen Yen Vu <vu.nguyen@hrboss.com>
2018-06-15 19:21:48 +0700

    move code and add front end vuejs

---
+++ funbet-backend/src/main/java/io/funbet/controller/HomeController.java
@@ -0,0 +1,13 @@
+package io.funbet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController
+{
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+}
