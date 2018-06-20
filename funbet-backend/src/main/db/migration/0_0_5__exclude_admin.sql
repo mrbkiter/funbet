c63cf7f5f0335fc3a915f796aca3b81ba8f1aaaa
Nguyen Yen Vu <vu.nguyen@hrboss.com>
2018-06-20 19:45:55 +0700

    remove redundant file

--- funbet-backend/src/main/db/migration/0_0_5__exclude_admin.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW finance_user_match_view AS
-SELECT m.tournament_id, m."id" AS match_id, m.bet_money, ua.id AS user_id, ua."name" AS name,
-ua.email AS email, um.bet_status, um.paid
-FROM user_account ua
-LEFT JOIN  user_match_bet um ON ua."id" = um.user_id
-LEFT JOIN "match" m ON  um.match_id = m."id"
-WHERE ua."role" != 'ADMIN';
