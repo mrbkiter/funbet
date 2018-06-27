f855c0a5e71d797d3623b8fec5e4d264a40077c0
Nguyen Yen Vu <vu.nguyen@hrboss.com>
2018-06-27 09:32:56 +0700

    only censors those already input result

--- funbet-backend/src/main/java/io/funbet/service/MatchService.java
+++ funbet-backend/src/main/java/io/funbet/service/MatchService.java
@@ -156,11 +156,12 @@ public class MatchService {
                     firstEle.setFollower2(firstEle.getFollower2() + 1);
             }
             //censor the selection of future matches
-            /*if(m.getSystemStartTime().isAfter(LocalDateTime.now()))
+            if(m.getSystemStartTime().isAfter(LocalDateTime.now())
+                    && m.getSelectedTeamId() != null)
             {
                 m.setSelectedTeamId(0);
                 m.setSelectedTeamName("CENSORED");
-            }*/
+            }
 
         });
         //set first element of each row
